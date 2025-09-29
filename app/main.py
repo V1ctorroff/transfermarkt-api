38d0f65093d01411ea9d54c45f662f857dce19f2
Victor Dev <152346903+V1ctorroff@users.noreply.github.com>
2025-09-29 13:42:56 -0300

    update main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
@@ -9,22 +10,26 @@ from starlette.responses import RedirectResponse
 from app.api.api import api_router
 from app.settings import settings
 
+# Configuração do rate limiting
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=[settings.RATE_LIMITING_FREQUENCY],
     enabled=settings.RATE_LIMITING_ENABLE,
 )
+
+# Inicialização do FastAPI
 app = FastAPI(title="Transfermarkt API")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 app.include_router(api_router)
 
-
+# Redirecionamento para /docs
 @app.get("/", include_in_schema=False)
 def docs_redirect():
     return RedirectResponse(url="/docs")
 
-
+# Execução do servidor com porta dinâmica
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
